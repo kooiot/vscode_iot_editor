7329a4ca80c57ecbfeeed5fff7f38fab26380190
Dirk Chang <dirk@kooiot.com>
2018-08-09 15:54:03 +0800

    Show timestamp in readable string Show packets in hex string mode

--- src/udp_con.ts
+++ src/udp_con.ts
@@ -27,6 +27,57 @@ export class UdpConn {
         });
         this.udpServer.bind(port);
     }
+    private formatDateTime(date: number): string {
+        // 2018-08-09 14:33:13.256
+        let tm = new Date(date);
+
+        //return string
+        var returnDate = "";
+        var dd = tm.getDate();
+        var mm = tm.getMonth() + 1; //because January is 0! 
+        var yyyy = tm.getFullYear();
+        var min = tm.getMinutes();
+        var hour = tm.getHours();
+        var sec = tm.getSeconds();
+        var ms = tm.getMilliseconds();
+
+        //Interpolation date
+        returnDate += yyyy;
+        if (mm < 10) {
+            returnDate += `-0${mm}`;
+        } else {
+            returnDate += `-${mm}`;
+        }
+        if (dd < 10) {
+            returnDate += `-0${dd}`;
+        } else {
+            returnDate += `-${dd}`;
+        }
+
+        if (hour < 10) {
+            returnDate += ` 0${hour}`;
+        } else {
+            returnDate += ` ${hour}`;
+        }
+        if (min < 10) {
+            returnDate += `:0${min}`;
+        } else {
+            returnDate += `:${min}`;
+        }
+        if (sec < 10) {
+            returnDate += `:0${sec}`;
+        } else {
+            returnDate += `:${sec}`;
+        }
+        if (ms < 10) {
+            returnDate += `.00${ms}`;
+        } else if(ms < 100) {
+            returnDate += `.0${ms}`;
+        } else {
+            returnDate += `.${ms}`;
+        }
+        return returnDate;
+    }
 
     private onData(msg: Buffer, rinfo: dgram.RemoteInfo): void {
         interface MSG {
@@ -39,20 +90,24 @@ export class UdpConn {
         if (obj.type === 'log') {
             let channel = this.client.LogChannel;
             if (channel) {
-                channel.appendLine(`[${rinfo.address}] ${obj.data.timestamp} [${obj.data.level}] [${obj.data.process}] ${obj.data.content} `);
+                let tm = this.formatDateTime(<number>obj.data.timestamp * 1000);
+                channel.appendLine(`[${rinfo.address}] [${tm}] [${obj.data.level}] [${obj.data.process}] ${obj.data.content} `);
             }
         } 
         if (obj.type === 'event') {
             let channel = this.client.LogChannel;
             if (channel) {
+                let tm = this.formatDateTime(<number>obj.data.timestamp * 1000);
                 let data_data = JSON.stringify(obj.data.data);
-                channel.appendLine(`[${rinfo.address}] ${obj.data.timestamp} [${obj.data.type}] [${obj.data.app}] [${obj.data.level}] ${obj.data.info} ${data_data}`);
+                channel.appendLine(`[${rinfo.address}] [${tm}] [${obj.data.type}] [${obj.data.app}] [${obj.data.level}] ${obj.data.info} ${data_data}`);
             }
         } 
         if (obj.type === 'comm') {
             let channel = this.client.CommChannel;
             if (channel) {
-                channel.appendLine(`[${rinfo.address}] ${obj.data.ts} [${obj.data.dir}] [${obj.data.sn}] ${obj.data.data} `);
+                let tm = this.formatDateTime(<number>obj.data.ts * 1000);
+                let data = new Buffer(obj.data.data, 'base64');
+                channel.appendLine(`[${rinfo.address}] [${tm}] [${obj.data.dir}] [${obj.data.sn}] ${data.toString('hex')} `);
             }
         }
     }
